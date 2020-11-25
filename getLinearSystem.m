3a1681597e10b98ce59d7fa5bb9cf70822e2fb8e
Acksell <axelen@kth.se>
2020-11-25 15:53:28 +0100

    Removed redundant code and added comments

--- getLinearSystem.m
+++ getLinearSystem.m
@@ -4,18 +4,12 @@
 function [A, bfunc]=getLinearSystem(R, dr, D, cooling_func)
     N=R/dr; % between dr and R-dr, add r=0 to diags after.
     rspace=linspace(0, R-dr, N);
-    
-    subdiag = zeros(N-1,1);
-    maindiag= zeros(N,1);
-    supdiag = zeros(N-1,1);
-    for i=1:N
-        
-    end
+
     subdiag = arrayfun(@(r_i) ((r_i-dr/2)/r_i)^2, rspace(2:end));
     maindiag= arrayfun(@(r_i) -( (r_i+dr/2)^2 + (r_i-dr/2)^2 )/((r_i)^2), rspace);
     supdiag = arrayfun(@(r_i) ((r_i+dr/2)/r_i)^2, rspace(2:end-1));
     subdiag=[subdiag 0]; % extend by 1 to match dimension, value doesnt matter.
-    supdiag=[0 0 supdiag]; % extend by 1 to match dimension, value doesnt matter.
+    supdiag=[0 0 supdiag]; % extend by 2 to match dimension, value doesnt matter.
     % set first row, this implements the neumann condition at r=0.
     maindiag(1) = -6;
     supdiag(2) = 6;
